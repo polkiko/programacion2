cb717a70b06065a60166997a07ec3f33f242eaaf
unknown <jesus.jerez1@gmail.com>
2019-03-17 01:42:10 +0100

    Sesion 04 - Primera revisión, falta por terminar

--- Sesion04/Racional.java
+++ Sesion04/Racional.java
@@ -1,23 +1,33 @@
+/**
+ * Clase Racional. Crea objetos del tipo racional, que son fracciones
+ * con su numerados y denominador. Además, contiene algunos métodos
+ * que nos permiten realizar algunas acciones con los mismos.
+ *
+ * ¡IMPORTANTE! En el fichero TestRacional.java, será donde esté el test
+ *
+ * @author Jesus Jerez
+ */
+
 class Racional{
-	
+
 	public int num;
 	public int den;
-	
+
 	public Racional(int num, int den){
 		this.num = num;
 		this.den = den;
 	}
-	
+
 	public String toString(){
 		return num + "/" + den;
 	}
-	
+
 	public void sum(Racional r2){
 		num = num * r2.den + r2.num * den;
 		den = den * r2.den;
 		simplificar();
 	}
-	
+
 	void simplificar(){
 		int i = 2;
 		while(this.num%i == 0 && this.den%i == 0){
--- Sesion04/TestRacional.java
+++ Sesion04/TestRacional.java
@@ -1,12 +1,23 @@
+/**
+ * Test de Racional.java. Este fichero nos servirá para hacer los test
+ * de nuestra clase 'Racional'
+ *
+ * ¡IMPORTANTE! Consulta el fichero Racional.java, será donde esté la clase
+ *
+ * @author Jesus Jerez
+ */
+
 class TestRacional{
 	public static void main(String[] args) {
-		Racional r1;
-		r1 = new Racional(2,6);
-		
+
+		Racional r1; // Defino la variable 'r1' del tipo 'Racional'
+		r1 = new Racional(2,6); // Instancio y asigno un Racional
+
 		Racional r2;
 		r2 = new Racional(1,3);
-		
-		r1.sum(r2);
-		System.out.println(r1);
+
+		// Consultar la clase Racional para conocer los métodos
+		r1.sum(r2); // Sumo el racional 'r2' a 'r1'
+		System.out.println(r1); // Imprimio r1
 	}
 }
