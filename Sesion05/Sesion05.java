181cfc802938fcbc34871bd61a828aa2e9e5c254
Polkiko <jesus.jerez1@gmail.com>
2019-03-27 10:38:18 +0100

    Sesion 05 - (En revisión)

--- Sesion05/Sesion05.java
+++ Sesion05/Sesion05.java
@@ -1,23 +1,31 @@
+/**
+ * En revisión
+ *
+ * ¡IMPORTANTE!
+ *
+ * @author Jesus Jerez
+ */
+
 class Sesion05{
-	
+
 	public static void main(String[] args){
 		Punto2D a;
 		Punto2D b;
-		
+
 		// Quiero que new Punto2D() cree una instancia que represente el (0,0)
-		
+
 		a = new Punto2D();
-		
-		// Quiero que new Punto2D(x,y) cree una instancia que represente el (x,y) 
-		
+
+		// Quiero que new Punto2D(x,y) cree una instancia que represente el (x,y)
+
 		b = new Punto2D(1, -1);
-		
+
 		//Voy a calcular la distancia entre los dos puntos
-		
+
 		double d = a.distancia(b);
-		
+
 		// Compruebo que está bien calculada
 		System.out.println(Math.sqrt(2) == d);
 	}
-	
+
 }
