dcb4d3c6c70617b37cdd11d1e11b2bf46cd43f0a
Polkiko <jesus.jerez1@gmail.com>
2019-03-27 10:39:51 +0100

    Sesion 06 - (En revisión)

--- Sesion06/Texas.java
+++ Sesion06/Texas.java
@@ -1,17 +1,23 @@
+/**
+ * En revisión
+ * ¡IMPORTANTE!
+ *
+ * @author Jesus Jerez
+ */
+
 class Texas{
-	
 	public static void main(String[] args){
 		Naipe[] cartas;
 		cartas = new Naipe[5];
-		
+
 		cartas[0] = new Naipe("picas", "as");
 		cartas[1] = new Naipe("picas", "rey");
 		cartas[2] = new Naipe("picas", "dama");
 		cartas[3] = new Naipe("picas", "valet");
 		cartas[4] = new Naipe("picas", "diez");
 
-		
+
 		System.out.println("Palo: " + cartas[0].palo());
-		
+
 	}
 }
