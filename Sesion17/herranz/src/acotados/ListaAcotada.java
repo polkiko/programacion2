d35eaa0acde71bc1f0bca02da4c384b790de45e0
Ángel Herranz <aherranz@fi.upm.es>
2019-04-07 09:42:57 +0200

    Implementación "vacía" de ListaAcotada:
    - sólo preocupado con que compile para poder escribir los tests
    - mínimo trabajo: constructor más métodos exigidos por el interface Lista

--- Sesion17/herranz/src/acotados/ListaAcotada.java
+++ Sesion17/herranz/src/acotados/ListaAcotada.java
@@ -1,4 +1,39 @@
 package acotados;
 
 public class ListaAcotada<E> implements Lista<E> {
+  /**
+   * Crea una lista acotada con una capacidad máxima indicada por
+   * capacidadMax
+   */
+  public ListaAcotada(int capacidadMax) {
+  }
+
+  public void add(int insertIndex, E element) {
+  }
+
+  public E get(int getIndex) {
+    return null;
+  }
+
+  public int indexOf(E search) {
+    return -1;
+  }
+
+  public boolean remove(E element) {
+    return false;
+  }
+
+  public void removeElementAt(int removalIndex) {
+  }
+
+  public void set(int insertIndex, E element) {
+  }
+
+  public int size() {
+    return 0;
+  }
+
+  public boolean isFull() {
+    return false;
+  }
 }
