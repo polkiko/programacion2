931de50e0695a95f8ab1fea227b337bda7507849
Ángel Herranz <aherranz@fi.upm.es>
2019-04-07 13:05:46 +0200

    Comenzamos con Contacto: implementación vacía de Contacto
    - para poder compilar los tests!

--- Sesion17/herranz/src/quepasa/Contacto.java
+++ Sesion17/herranz/src/quepasa/Contacto.java
@@ -1,4 +1,45 @@
 package quepasa;
 
+import acotados.Lista;
+
 public class Contacto {
+  /**
+   * Constructor de contacto vacío.
+   */
+  public Contacto() {
+  }
+
+  /**
+   * Devuelve el nombre del contacto o null si no tiene nombre.
+   */
+  public String nombre() {
+    return null;
+  }
+
+  /**
+   * Se envía el mensaje al contacto.
+   */
+  public void enviar(String mensaje) {
+  }
+
+  /**
+   * Devuelve todos los mensajes enviados a un usuario.
+   */
+  public Lista<String> mensajes() {
+    return null;
+  }
+
+  /**
+   * Devuelve el último mensaje enviado (null si no hay mensajes).
+   */
+  public String ultimoMensaje() {
+    return null;
+  }
+
+  /**
+   * Devuelve el número de mensajes enviados al contacto.
+   */
+  public int nMensajes() {
+    return 0;
+  }
 }
