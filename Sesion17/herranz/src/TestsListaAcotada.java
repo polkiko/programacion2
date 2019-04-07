587895301f4bbefadc50be198c625c9a1b481c1e
Ángel Herranz <aherranz@fi.upm.es>
2019-04-07 09:48:58 +0200

    Primeras pruebas, muy sencillo:
    - añadimos un único elemento
    - comprobamos tamaño
    - no lleno
    - borrados

--- Sesion17/herranz/src/TestsListaAcotada.java
+++ Sesion17/herranz/src/TestsListaAcotada.java
@@ -7,5 +7,18 @@ public class TestsListaAcotada {
     Lista<String> l = new ListaAcotada<String>(N);
     assert l.size() == 0 : "La lista debería estar vacía al crearla";
     assert !l.isFull() : "La lista no debería estar llena  al crearla";
+
+    // Prueba sencilla añadiendo un elemento y luego borrándolo por el
+    // camino comprobamos propiedades básicas
+    boolean borrado;
+    l.add(0,"Hola");
+    assert l.size() == 1;
+    assert !l.isFull();
+    assert "Hola".equals(l.get(0));
+    borrado = l.remove("Adios");
+    assert !borrado;
+    borrado = l.remove("Hola");
+    assert borrado;
+
   }
 }
