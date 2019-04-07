eb5339126248aa590edd965b7a44bbe77da4bcd3
Ángel Herranz <aherranz@fi.upm.es>
2019-04-07 13:11:01 +0200

    Tests sobre un contacto vacío

--- Sesion17/herranz/src/TestsContacto.java
+++ Sesion17/herranz/src/TestsContacto.java
@@ -3,6 +3,11 @@ import quepasa.Contacto;
 public class TestsContacto {
   public static void main(String[] args) {
     Contacto c = new Contacto();
-    assert c != null;
+
+    // Prueba para ver que un contacto vacío no tiene nada
+    assert c.nombre() == null;
+    assert c.avatar() == null;
+    assert c.nMensajes() == 0;
+
   }
 }
