2497ef3af23260599a3a5caf3d1841f1e647b3fa
Ángel Herranz <aherranz@fi.upm.es>
2019-04-07 11:06:24 +0200

    Array de object:
    - utilizamos un array de Object como representación de los elementos de la lista

--- Sesion17/herranz/src/acotados/ListaAcotada.java
+++ Sesion17/herranz/src/acotados/ListaAcotada.java
@@ -2,10 +2,20 @@ package acotados;
 
 public class ListaAcotada<E> implements Lista<E> {
   /**
+   * Array con el que representamos los elementos en la lista. La
+   * declaración debería ser E[] elementos pero Java no permite luego
+   * crear un array donde el tipo de los elementos sea un parámetro
+   * genérico (error: generic array creation). Por eso se declara como
+   * array de objetos.
+   */
+  private Object[] elementos ;
+
+  /**
    * Crea una lista acotada con una capacidad máxima indicada por
    * capacidadMax
    */
   public ListaAcotada(int capacidadMax) {
+    elementos = new Object[capacidadMax];
   }
 
   public void add(int insertIndex, E element) {
