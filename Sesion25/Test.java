0ac21a9acd718565d5bee7baa36c37f8f59c91dd
Jesus Jerez <jesus.jerez1@gmail.com>
2019-05-31 14:19:51 +0200

    Sesion 25 - Listas

---
+++ Sesion25/Test.java
@@ -0,0 +1,16 @@
+class Test{
+  public static void main (String[] args){
+
+    Nodo<String> nuevo = new Nodo<String>(s);
+    if (l == null) {
+      l = nuevo;
+    } else {
+      Nodo<String> aux = l;
+      while (aux.siguiente != null) {
+        aux = aux.siguiente;
+      }
+        aux = aux;
+        aux.siguiente = nuevo;
+      }
+  }
+}
